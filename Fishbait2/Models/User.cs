e203686bf905552d08e11470076fb8ae51694e92
TimonC <58507497+TimonC@users.noreply.github.com>
2020-04-06 16:03:40 +0200

    Data annotations toegevoegd + Viewimports informatie

--- Fishbait2/Models/User.cs
+++ Fishbait2/Models/User.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Fishbait2.Models
 {
     public class User
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You have to put in an username!")]
+        [Display(Name = "Username:")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "You have to enter a password!")]
+        [Display(Name = "Password:")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "You have to enter an e-mail!")]
+        [Display(Name = "E-Mail:")]
+        public string Mail { get; set; }
     }
 }
