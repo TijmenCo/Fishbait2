f4c543f16ef8bd51c7267170c812660f36fc8ae5
TimonC <58507497+TimonC@users.noreply.github.com>
2020-05-15 13:59:48 +0200

    Factory DAL afgemaakt

---
+++ DALFactories/PostDBFactory.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DALFactories
+{
+    public static class PostDBFactory
+    {
+        public static IPostDto GetPost()
+        {
+            return new PostDto();
+        }
+    }
+}
