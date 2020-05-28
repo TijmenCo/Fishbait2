a903ec5e4c7014f388e70a764c0c5a7ddd6e4f12
TimonC <58507497+TimonC@users.noreply.github.com>
2020-05-28 11:12:00 +0200

    Begin Unit Tests

---
+++ FishbaitUnitTests/LogicTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DAL.Models;
+using DALFactories;
+using Factories;
+using Fishbait2.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FishbaitUnitTests
+{
+    [TestClass]
+    public class LogicTests
+    {
+        [TestMethod]
+        public void AddPostTest()
+        {
+            IPost postDto = PostFactory.GetPost();
+            var mock = new Mock<IPost>();
+            postDto.id = 1;
+            postDto.title = "Lepel";
+            postDto.description = "Nieuwe soort lepel";
+            postDto.image = "lepel.png";
+            postDto.tag = "Tech";
+            mock.Setup(foo => foo.AddPost(postDto)).Returns("Data save Successfully");
+        }
+    }
+}
---
+++ LogicTests/PostTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fishbait2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fishbait2.Models.Tests
+{
+    [TestClass()]
+    public class PostTests
+    {
+        [TestMethod()]
+        public void AddPostTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
