c7eb41d656b5f9463d27a186f3078d8140ea58a6
TimonC <58507497+TimonC@users.noreply.github.com>
2020-06-10 12:51:40 +0200

    GetPostID afgemaakt

--- FishbaitUnitTests/LogicTests.cs
+++ FishbaitUnitTests/LogicTests.cs
@@ -21,10 +21,11 @@ namespace FishbaitUnitTests
         public void TestInitialize()
         {
         }
+        /*
         [TestMethod]
         public void AddPost_AddAPost_ReturnsDataSaveSuccessfully()
         {
-            /*
+            
             var dalMock = new Mock<IPostDBAccesLayer>();
             var postMock = new Mock<IPostDBAccesLayer>();
             dalMock.Setup(m => m.AddPost(It.IsAny<IPostDto>())).Returns("Data save succesfull");
@@ -43,26 +44,9 @@ namespace FishbaitUnitTests
 
             // Verify that the mock was invoked
             //    mock.VerifyGet(x => x.title);
-            */
-        }
-        [TestMethod]
-        public void AddPost_AddAPostGetsCorrectlySaved_ReturnsDataSaveFailed()
-        {
-            /*
-            var mock = new Mock<IPostDBAccesLayer>();
-            mock.Setup(m => m.AddPost()).Returns("Data save Failed");
-            //Configure Mocks
-           mock.Setup(m => m.title).Returns("");
-           mock.Setup(m => m.description).Returns("Nieuwe soort lepel");
-           mock.Setup(m => m.image).Returns("lepel.png");
-            mock.Setup(m => m.tag).Returns("Tech");
-            Save Result
           
-            //Check if add went succesfully
-            Assert.AreEqual(result, "Data save Failed");
-            */
-
         }
+    */
         [TestMethod]
         public void GetPosts_PostsGetCorrectlySavedFromDB_ReturnsTrue()
         {
@@ -78,28 +62,15 @@ namespace FishbaitUnitTests
             Assert.AreEqual(gottenPosts.Count(), posts.Count());
         }
         [TestMethod]
-        public void DeletePost_PostGetsDeleted_ReturnsTrue()
-        {
-            /*
-            var mock = new Mock<IPost>();
-            mock.Setup(m => m.DeletePost(mock.Object.id)).Returns("Data deletion Succes");
-            mock.Setup(m => m.id).Returns(16);
-            var result = mock.Object.DeletePost(mock.Object.id);
-            Assert.AreEqual(result, "Data deletion Succes");
-            //Save Result
-
-            //Check if GetPosts went succesfully
-            //Assert.AreEqual(exists, true);
-            */
-        }
-        [TestMethod]
         public void GetPostID_PostGetCorrectlyWithID_ReturnsTrue()
         {
             List<IPostDto> posts = GivePostsDto();
             var mock = new Mock<IPostDBAccesLayer>();
             mock.Setup(m => m.GetPosts()).Returns(posts);
             var post = new Post(mock.Object);
+            var resultID = posts.Where(x => x.id == 3).ToList();
             var result = post.GetPostID(3);
+            Assert.AreEqual(result.id, resultID[0].id);
         }
 
             public List<IPostDto> GivePostsDto()
@@ -111,24 +82,24 @@ namespace FishbaitUnitTests
                 postDto[i] = PostDBFactory.GetPost();
              }
             postDto[0].id = 1;
-            postDto[0].title = "Help";
-            postDto[0].description = "Beschrijving";
-            postDto[0].image = "foto.png";
-            postDto[0].tag = "Tech";
+            postDto[0].title = "Nieuwe schep!";
+            postDto[0].description = "Een nieuwe soort schep!";
+            postDto[0].image = "schep.png";
+            postDto[0].tag = "Nature";
             posts.Add(postDto[0]);
 
             postDto[1].id = 2;
-            postDto[1].title = "Help";
-            postDto[1].description = "Beschrijving";
-            postDto[1].image = "foto.png";
+            postDto[1].title = "Nieuwe Auto!";
+            postDto[1].description = "Een snellere auto!";
+            postDto[1].image = "auto.png";
             postDto[1].tag = "Tech";
             posts.Add(postDto[1]);
 
             postDto[2].id = 3;
-            postDto[2].title = "Help";
-            postDto[2].description = "Beschrijving";
-            postDto[2].image = "foto.png";
-            postDto[2].tag = "Tech";
+            postDto[2].title = "Nieuwe soort pen!";
+            postDto[2].description = "Deze pen heeft een nieuwe kleur!";
+            postDto[2].image = "pen.png";
+            postDto[2].tag = "Creative";
             posts.Add(postDto[2]);
             return posts;
 
