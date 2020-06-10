6718131657d26f315702c860e89887268ceb6e3e
TimonC <58507497+TimonC@users.noreply.github.com>
2020-06-10 15:09:47 +0200

    Begin Gemaakt aan notificationTests

---
+++ FishbaitUnitTests/NotificationTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishbaitUnitTests
+{
+    [TestClass]
+    public class NotificationTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+        [TestMethod]
+        public void GetUpdatePosts_UpdatePostsGetCorrectlySavedFromDB_ReturnsTrue()
+        {
+        }
+    }
+}
